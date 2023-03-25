d12d48294a09656808ce9f61512a3aaab8ba56f7
sitedetest0allex <128940360+sitedetest0allex@users.noreply.github.com>
2023-03-25 20:36:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,31 @@
-// definindo variáveis do jogo
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var tamanhoCauda = 3;
-var tamanhoCobrinha = 20;
-var cobrinha = [];
-var comida = {x: 0, y: 0};
-var pontuacao = 0;
-var direcao = "direita";
+// variáveis globais
+var currentPlayer = "X";
+var cells = document.querySelectorAll(".cell");
+var status = document.querySelector(".status");
 
-// função para criar a cobrinha
-function criarCobrinha() {
-	for (var i = tamanhoCauda - 1; i >= 0; i--) {
-		cobrinha.push({x: i, y: 0});
-	}
+// função para reiniciar o jogo
+function reset() {
+	currentPlayer = "X";
+	cells.forEach(function(cell) {
+		cell.innerHTML = "";
+		cell.style.backgroundColor = "#ddd";
+		cell.addEventListener("click", handleClick, false);
+	});
+	status.innerHTML = "";
 }
 
-// função para desenhar a cobrinha na tela
-function desenharCobrinha() {
-	for (var i = 0; i < cobrinha.length; i++) {
-		ctx.fillStyle = "#fff";
-		ctx.fillRect(cobrinha[i].x * tamanhoCobrinha, cobrinha[i].y * tamanhoCobrinha, tamanhoCobrinha, tamanhoCobrinha);
-	}
-}
+// função para verificar se houve vencedor
+function checkWinner() {
+	var winningCombinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
 
-// função para mover a cobrinha
-function moverCobrinha() {
-	var cabecaX = cobrinha[0].x;
-	var cabecaY = cobrinha[0].y;
-
-	if (direcao == "direita") cabecaX++;
-	else if (direcao == "esquerda") cabecaX--;
-	else if (direcao == "cima") cabecaY--;
-	else if (direcao == "baixo") cabecaY++;
-
-	// verifica se a cobrinha saiu da tela
-	if (cabecaX < 0 || cabecaX >= canvas.width / tamanhoCobrinha || cabecaY < 0 || cabecaY >= canvas.height / tamanhoCobrinha) {
-		// fim de jogo
-		clearInterval(intervalo);
-		alert("Fim de jogo! Sua pontuação foi: " + pontuacao);
-	}
-
-	// verifica se a cobrinha comeu a comida
-	if (cabecaX == comida.x && cabecaY == comida.y) {
-		pontuacao++;
-		tamanhoCauda++;
-		criarComida();
-	} else {
-		cobrinha.pop();
-	}
-
-	var novaCabeca = {x: cabecaX, y: cabecaY};
-	cobrinha.unshift(novaCabeca);
-}
-
-// função para criar a comida
-function criarComida() {
-	comida.x = Math.floor(Math.random() * (canvas.width / tamanhoCobrinha));
-	comida.y = Math.floor(Math.random() * (canvas.height / tamanhoCobrinha));
-
-	for (var i = 0; i < cobrinha.length; i++) {
-		if (cobrinha[i].x == comida.x && cobrinha[i].y == comida.y) {
-			criarComida();
-		}
-	}
-
-	ctx.fillStyle = "#f00";
-	ctx.fillRect(comida.x * tamanhoCobrinha
+	for (var i = 0; i < winningCombinations.length; i++) {
+		var a = cells[winningCombinations[i
