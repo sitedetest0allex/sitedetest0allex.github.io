8e28e862d07ef53fc1af9ed7bdf88209cf255636
sitedetest0allex <128940360+sitedetest0allex@users.noreply.github.com>
2023-03-25 20:27:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let guessCount = 0;
-let guessInput = document.getElementById('guessInput');
-let guessButton = document.getElementById('guessButton');
-let feedback = document.getElementById('feedback');
-let info = document.getElementById('info');
+// definindo variáveis do jogo
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var tamanhoCauda = 3;
+var tamanhoCobrinha = 20;
+var cobrinha = [];
+var comida = {x: 0, y: 0};
+var pontuacao = 0;
+var direcao = "direita";
 
-function checkGuess() {
-  let userGuess = Number(guessInput.value);
-  guessCount++;
-  if (userGuess === randomNumber) {
-    feedback.textContent = 'Parabéns! Você acertou em ' + guessCount + ' tentativas!';
-    info.style.display = 'none';
-    guessInput.disabled = true;
-    guessButton.disabled = true;
-  } else if (guessCount === 10) {
-    feedback.textContent = 'Game over. O número correto era ' + randomNumber + '.';
-    info.style.display = 'none';
-    guessInput.disabled = true;
-    guessButton.disabled = true;
-  } else {
-    feedback.textContent = 'Tente novamente.';
-    if (userGuess < randomNumber) {
-      info.textContent = 'O número é maior que ' + userGuess + '.';
-    } else {
-      info.textContent = 'O número é menor que ' + userGuess + '.';
-    }
-  }
-  guessInput.value = '';
-  guessInput.focus();
+// função para criar a cobrinha
+function criarCobrinha() {
+	for (var i = tamanhoCauda - 1; i >= 0; i--) {
+		cobrinha.push({x: i, y: 0});
+	}
 }
 
-guessButton.addEventListener('click', checkGuess);
+// função para desenhar a cobrinha na tela
+function desenharCobrinha() {
+	for (var i = 0; i < cobrinha.length; i++) {
+		ctx.fillStyle = "#fff";
+		ctx.fillRect(cobrinha[i].x * tamanhoCobrinha, cobrinha[i].y * tamanhoCobrinha, tamanhoCobrinha, tamanhoCobrinha);
+	}
+}
 
+// função para mover a cobrinha
+function moverCobrinha() {
+	var cabecaX = cobrinha[0].x;
+	var cabecaY = cobrinha[0].y;
+
+	if (direcao == "direita") cabecaX++;
+	else if (direcao == "esquerda") cabecaX--;
+	else if (direcao == "cima") cabecaY--;
+	else if (direcao == "baixo") cabecaY++;
+
+	// verifica se a cobrinha saiu da tela
+	if (cabecaX < 0 || cabecaX >= canvas.width / tamanhoCobrinha || cabecaY < 0 || cabecaY >= canvas.height / tamanhoCobrinha) {
+		// fim de jogo
+		clearInterval(intervalo);
+		alert("Fim de jogo! Sua pontuação foi: " + pontuacao);
+	}
+
+	// verifica se a cobrinha comeu a comida
+	if (cabecaX == comida.x && cabecaY == comida.y) {
+		pontuacao++;
+		tamanhoCauda++;
+		criarComida();
+	} else {
+		cobrinha.pop();
+	}
+
+	var novaCabeca = {x: cabecaX, y: cabecaY};
+	cobrinha.unshift(novaCabeca);
+}
+
+// função para criar a comida
+function criarComida() {
+	comida.x = Math.floor(Math.random() * (canvas.width / tamanhoCobrinha));
+	comida.y = Math.floor(Math.random() * (canvas.height / tamanhoCobrinha));
+
+	for (var i = 0; i < cobrinha.length; i++) {
+		if (cobrinha[i].x == comida.x && cobrinha[i].y == comida.y) {
+			criarComida();
+		}
+	}
+
+	ctx.fillStyle = "#f00";
+	ctx.fillRect(comida.x * tamanhoCobrinha
