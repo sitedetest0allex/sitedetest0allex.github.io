1b041f5ec5cb3be87d7cebc1671c9820b62a805e
sitedetest0allex <128940360+sitedetest0allex@users.noreply.github.com>
2023-03-27 07:57:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const link = input.value;
-  window.location.href = `https://downloadgram.net/download?vid=${link}`;
-});
 
